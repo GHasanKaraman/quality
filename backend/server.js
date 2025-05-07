b620c397a86cc7120e829d52b5a77b720ed322c5
Gurkan Karaman <40955470+GHasanKaraman@users.noreply.github.com>
2025-05-06 21:48:22 -0400

    default and 404 pages are added

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,17 @@ const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
 
 const authentication = require("./routes/authentication.js");
 const { logger } = require("./utility/logger");
 const { connectDB } = require("./utility/db.js");
 const { verifyJWT } = require("./utility/JWT.js");
 
-require("dotenv").config();
-
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-
-app.use(express.json());
 
 //mongoose connection
 const db = connectDB();
@@ -22,13 +20,15 @@ const db = connectDB();
 db.on("error", console.error.bind(console, "connection error:"));
 
 db.once("open", function () {
+  app.use("/", express.static(path.join(__dirname, "public")));
+
   //Accepts requests from only react client
   app.use(
     cors({
       origin: ["http://localhost:3000", "http://10.12.11.193:3000"],
       methods: ["GET", "POST"],
       credentials: true,
-    }),
+    })
   );
 
   //parse requests to JSON
@@ -41,22 +41,25 @@ db.once("open", function () {
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    }),
+    })
   );
 
   //Logging requests
   app.use(logger());
 
-  app.use((req, res, next) => {
-    if ("OPTIONS" === req.method) {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
+  app.use(express.json());
 
-  app.get("/", async (req, res) => {
-    res.send("Server is running...");
+  app.use("/", require("./routes/root.js"));
+
+  app.all(/.*/, (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+      res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+      res.json({ message: "404 Not Found" });
+    } else {
+      res.type("txt").send("404 Not Found");
+    }
   });
 
   app.use("/", authentication);
@@ -70,7 +73,7 @@ db.once("open", function () {
   app.listen(PORT, () => {
     console.log(
       "\x1b[33m%s\x1b[0m",
-      "mongo connection established successfully!",
+      "mongo connection established successfully!"
     );
     console.log("\x1b[32m%s\x1b[0m", "Listening on port " + process.env.PORT);
   });
