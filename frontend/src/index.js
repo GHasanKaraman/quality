191342ae1e55381f85fd34b8bbb9077579ad2510
Gurkan Karaman <40955470+GHasanKaraman@users.noreply.github.com>
2025-05-14 22:10:58 -0400

    redux init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,24 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
 import { SnackbarProvider } from "notistack";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SnackbarProvider
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-  >
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </SnackbarProvider>
+  <Provider store={store}>
+    <SnackbarProvider
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
+  </Provider>
 );
